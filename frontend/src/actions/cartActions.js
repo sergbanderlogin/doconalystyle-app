10c58df52a330e40f731c76a5141dd836a44e436
sergbanderlogin <sergbanderlogin@gmail.com>
2020-11-11 10:37:01 +0200

    create removeFromCart constants, actions and reducers, use action in CartScreen.js

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,8 +1,13 @@
 import Axios from 'axios';
-import { CART_ADD_ITEM } from '../constants/cartConstants';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM
+} from '../constants/cartConstants';
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
-  const { data } = await Axios.get(`/api/products/${productId}`);
+  const {
+    data
+  } = await Axios.get(`/api/products/${productId}`);
   dispatch({
     type: CART_ADD_ITEM,
     payload: {
@@ -13,6 +18,14 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
       product: data._id,
       qty,
     },
+  });
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (productId) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: productId
   });
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
