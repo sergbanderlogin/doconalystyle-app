7d08ece0346c8421d27f87cc17cae1c8deb45bd4
sergbanderlogin <sergbanderlogin@gmail.com>
2020-11-15 23:40:09 +0200

    create signin constants, actions and reducers, add reducer to store.js, use action in SigninScreen.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,25 +1,32 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux'
 import thunk from 'redux-thunk'
-import { cartReducer } from './reducers/cartReducers'
-import { productDetailsReducer, productListReducer } from './reducers/productReducers'
+import {cartReducer} from './reducers/cartReducers'
+import {productDetailsReducer, productListReducer} from './reducers/productReducers'
+import {userSigninReducer} from "./reducers/userReducers";
 
 const initialState = {
+  userSignin: {
+    userinfo: localStorage.getItem('userInfo')
+        ? JSON.parse(localStorage.getItem('userInfo'))
+        : null,
+  },
   cart: {
     cartItems: localStorage.getItem('cartItems')
-      ? JSON.parse(localStorage.getItem('cartItems'))
-      : []
+        ? JSON.parse(localStorage.getItem('cartItems'))
+        : []
   }
 }
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userSignin: userSigninReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
-  reducer,
-  initialState,
-  composeEnhancer(applyMiddleware(thunk))
+    reducer,
+    initialState,
+    composeEnhancer(applyMiddleware(thunk))
 )
 
 export default store
