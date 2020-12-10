16b3d62460ef63f3d9d6b8bb560abf3795d93dc6
sergbanderlogin <sergbanderlogin@gmail.com>
2020-12-11 00:34:23 +0200

    Create Product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,3 +1,10 @@
+import {
+  PRODUCT_CREATE_FAIL,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_RESET,
+  PRODUCT_CREATE_SUCCESS
+} from "../constants/productConstants";
+
 const {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -8,33 +15,48 @@ const {
 } = require('../constants/productConstants');
 
 export const productListReducer = (
-  state = { loading: true, products: [] },
-  action
+    state = {loading: true, products: []},
+    action
 ) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-      return { loading: true };
+      return {loading: true};
     case PRODUCT_LIST_SUCCESS:
-      return { loading: false, products: action.payload };
+      return {loading: false, products: action.payload};
     case PRODUCT_LIST_FAIL:
-      return { loading: false, error: action.payload };
+      return {loading: false, error: action.payload};
     default:
       return state;
   }
 };
 
 export const productDetailsReducer = (
-  state = { product: {}, loading: true },
-  action
+    state = {product: {}, loading: true},
+    action
 ) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
-      return { loading: true };
+      return {loading: true};
     case PRODUCT_DETAILS_SUCCESS:
-      return { loading: false, product: action.payload };
+      return {loading: false, product: action.payload};
     case PRODUCT_DETAILS_FAIL:
-      return { loading: false, error: action.payload };
+      return {loading: false, error: action.payload};
     default:
       return state;
   }
 };
+
+export const productCreateReducer  = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REQUEST:
+      return {loading: true};
+    case PRODUCT_CREATE_SUCCESS:
+      return {loading: false, success: true, product: action.payload};
+    case PRODUCT_CREATE_FAIL:
+      return {loading: false, error: action.payload};
+    case PRODUCT_CREATE_RESET:
+      return {};
+    default:
+      return state;
+  }
+}
