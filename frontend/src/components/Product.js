cf9d57085fe1670ccbf23941e11ea85d731bd407
sergbanderlogin <sergbanderlogin@gmail.com>
2021-01-09 23:54:44 +0200

    Force Order Items From One Seller

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Rating from './Rating';
-
 export default function Product(props) {
-  const {product} = props;
+  const { product } = props;
   return (
       <div key={product._id} className="card">
         <Link to={`/product/${product._id}`}>
-          <img className="medium" src={product.image} alt={product.name}/>
+          <img className="medium" src={product.image} alt={product.name} />
         </Link>
         <div className="card-body">
           <Link to={`/product/${product._id}`}>
@@ -16,15 +15,14 @@ export default function Product(props) {
           <Rating
               rating={product.rating}
               numReviews={product.numReviews}
-          />
-
+          ></Rating>
           <div className="row">
             <div className="price">${product.price}</div>
-            {/*<div>
+            <div>
               <Link to={`/seller/${product.seller._id}`}>
-                {product.seller.seller.name}
+                {product.seller.name}
               </Link>
-            </div>*/}
+            </div>
           </div>
         </div>
       </div>
