ef9f55670a62dccd8113ca2c9ee79be4bbc7c064
sergbanderlogin <sergbanderlogin@gmail.com>
2021-01-10 03:23:03 +0200

    Force Order Items From One Seller

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,32 +2,77 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import Product from '../models/productModel.js';
-import {isAdmin, isAuth, isSellerOrAdmin} from '../utils.js';
+import User from '../models/userModel.js';
+import { isAdmin, isAuth, isSellerOrAdmin } from '../utils.js';
 
 const productRouter = express.Router();
-
 productRouter.get(
     '/',
     expressAsyncHandler(async (req, res) => {
+      const name = req.query.name || '';
+      const category = req.query.category || '';
       const seller = req.query.seller || '';
-      const sellerFilter = seller ? {seller} : {};
-      const products = await Product.find({ ...sellerFilter }).populate(
-          'seller',
-          'seller.name seller.logo'
-      );
+      const order = req.query.order || '';
+      const min =
+          req.query.min && Number(req.query.min) !== 0 ? Number(req.query.min) : 0;
+      const max =
+          req.query.max && Number(req.query.max) !== 0 ? Number(req.query.max) : 0;
+      const rating =
+          req.query.rating && Number(req.query.rating) !== 0
+              ? Number(req.query.rating)
+              : 0;
+      const nameFilter = name ? { name: { $regex: name, $options: 'i' } } : {};
+      const sellerFilter = seller ? { seller } : {};
+      const categoryFilter = category ? { category } : {};
+      const priceFilter = min && max ? { price: { $gte: min, $lte: max } } : {};
+      const ratingFilter = rating ? { rating: { $gte: rating } } : {};
+      const sortOrder =
+          order === 'lowest'
+              ? { price: 1 }
+              : order === 'highest'
+              ? { price: -1 }
+              : order === 'toprated'
+                  ? { rating: -1 }
+                  : { _id: -1 };
+      const products = await Product.find({
+        ...sellerFilter,
+        ...nameFilter,
+        ...categoryFilter,
+        ...priceFilter,
+        ...ratingFilter,
+      })
+          .populate('seller', 'seller.name seller.logo')
+          .sort(sortOrder);
       res.send(products);
     })
 );
-
-
+productRouter.get(
+    '/categories',
+    expressAsyncHandler(async (req, res) => {
+      const categories = await Product.find().distinct('category');
+      res.send(categories);
+    })
+);
 productRouter.get(
     '/seed',
     expressAsyncHandler(async (req, res) => {
       // await Product.remove({});
-      const createdProducts = await Product.insertMany(data.products);
-      res.send({createdProducts});
+      const seller = await User.findOne({ isSeller: true });
+      if (seller) {
+        const products = data.products.map((product) => ({
+          ...product,
+          seller: seller._id,
+        }));
+        const createdProducts = await Product.insertMany(products);
+        res.send({ createdProducts });
+      } else {
+        res
+            .status(500)
+            .send({ message: 'No seller found. first run /api/users/seed' });
+      }
     })
 );
+
 productRouter.get(
     '/:id',
     expressAsyncHandler(async (req, res) => {
@@ -38,7 +83,7 @@ productRouter.get(
       if (product) {
         res.send(product);
       } else {
-        res.status(404).send({message: 'Product Not Found'});
+        res.status(404).send({ message: 'Product Not Found' });
       }
     })
 );
@@ -60,7 +105,7 @@ productRouter.post(
         description: 'sample description',
       });
       const createdProduct = await product.save();
-      res.send({message: 'Product Created', product: createdProduct});
+      res.send({ message: 'Product Created', product: createdProduct });
     })
 );
 productRouter.put(
@@ -79,9 +124,9 @@ productRouter.put(
         product.countInStock = req.body.countInStock;
         product.description = req.body.description;
         const updatedProduct = await product.save();
-        res.send({message: 'Product Updated', product: updatedProduct});
+        res.send({ message: 'Product Updated', product: updatedProduct });
       } else {
-        res.status(404).send({message: 'Product Not Found'});
+        res.status(404).send({ message: 'Product Not Found' });
       }
     })
 );
@@ -93,9 +138,41 @@ productRouter.delete(
       const product = await Product.findById(req.params.id);
       if (product) {
         const deleteProduct = await product.remove();
-        res.send({message: 'Product Deleted', product: deleteProduct});
+        res.send({ message: 'Product Deleted', product: deleteProduct });
       } else {
-        res.status(404).send({message: 'Product Not Found'});
+        res.status(404).send({ message: 'Product Not Found' });
+      }
+    })
+);
+productRouter.post(
+    '/:id/reviews',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+      const productId = req.params.id;
+      const product = await Product.findById(productId);
+      if (product) {
+        if (product.reviews.find((x) => x.name === req.user.name)) {
+          return res
+              .status(400)
+              .send({ message: 'You already submitted a review' });
+        }
+        const review = {
+          name: req.user.name,
+          rating: Number(req.body.rating),
+          comment: req.body.comment,
+        };
+        product.reviews.push(review);
+        product.numReviews = product.reviews.length;
+        product.rating =
+            product.reviews.reduce((a, c) => c.rating + a, 0) /
+            product.reviews.length;
+        const updatedProduct = await product.save();
+        res.status(201).send({
+          message: 'Review Created',
+          review: updatedProduct.reviews[updatedProduct.reviews.length - 1],
+        });
+      } else {
+        res.status(404).send({ message: 'Product Not Found' });
       }
     })
 );
